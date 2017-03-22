45207a41ef1fc02b45b9b153ab946ca063c1fb64
Simone Gentili <sensorario@gmail.com>
2017-03-23 00:26:02 +0100

    add new Centerln method

--- center.go
+++ center.go
@@ -13,6 +13,18 @@ func Center(s string) {
 	termbox.Close()
 	fmt.Printf(
 		fmt.Sprintf("%%-%ds", w/2),
+		fmt.Sprintf(fmt.Sprintf("%%%ds", w/2+len(s)/2), s),
+	)
+}
+
+func Centerln(s string) {
+	if err := termbox.Init(); err != nil {
+		panic(err)
+	}
+	w, _ := termbox.Size()
+	termbox.Close()
+	fmt.Printf(
+		fmt.Sprintf("%%-%ds", w/2),
 		fmt.Sprintf(fmt.Sprintf("%%%ds\n", w/2+len(s)/2), s),
 	)
 }
